09b4dfe914b16c1d0b31cde56a568c0ecdc6cd20
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-08-16 15:55:26 +0530

    Update AffineCipher.c

--- AffineCipher.c
+++ AffineCipher.c
@@ -1,16 +1,22 @@
+/*
+ * Decryption:Implementing Affine cipher
+ * DATE:16-08-2014
+ * varsion:1.0
+ * */
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<math.h>
-#define MAXSIZE 8
+#define MAXSIZE 80
 int main()
 {
-	char plain[80],cipher[80];
-	int i,k,a,b,s,t,x,y,inverse[26];
+	char plain[MAXSIZE],cipher[MAXSIZE];
+	int i,j,a,b,c,d,x,y,inverse[26],len;
 	inverse[0]=0;
 	inverse[1]=1;
 	inverse[3]=9;
-	inverse[5]=21;inverse[7]=15;
+	inverse[5]=21;
+	inverse[7]=15;
 	inverse[9]=3;
 	inverse[11]=19;
 	inverse[15]=7;
@@ -21,42 +27,55 @@ int main()
 	inverse[25]=25;
 	printf("Encrypted function is (ax+b)mod 26\n");
 	printf("Please Enter Plain text: \n");
-	for(i=0;i<MAXSIZE;i++)
+	gets(plain);
+
+	len=strlen(plain);
+	for(i=0;i<len;i++)
 	{
-		scanf("%c",&plain[i]);
+		if(isalpha(plain[i]))//allowing only charecters to be the plain text.
+			continue;
+		else
+		{
+			printf("Ente only characters\n");
+			return -1;
+		}
 	}
 	printf("\n");
 	printf("Enter a,b values\n");
 	scanf("%d %d",&a,&b);
-	if(a%2==0||a==13||a>25||b>25)
+	if(a%2==0||a==13||a>25||b>25)//checking for valid a,b values.
+	{
 		printf("(a,b)=(%d %d) is invalid\n",a,b);
+		exit(0);
+	}
 	else
 	{
-		s=inverse[a];
-		t=(s*(26-b))%26;
+		c=inverse[a];
+		d=(c*(26-b))%26;
 	}
-	for(k=0;k<MAXSIZE;k++)
+	printf("plain text is:\t");
+	for(i=0;i<len;i++)
+		printf("%c",plain[i]);
+	printf("\nEncrypting...\n");
+	for(j=0;j<len;j++)
 	{
-		x=(int)plain[k]-(int)'a';
-		y=(a*x+b)%26;
-		cipher[k]=(char)(y+65);
+		x=(int)plain[j]-(int)'a';//assigning 0-25 values for entered chatecters
+		y=(a*x+b)%26;//calculating encryption function
+		cipher[j]=(char)(y+65);//converting cipher into uppercase
 	}
-	printf("cipher text is:\n");
-	for(i=0;i<MAXSIZE;i++)
+	printf("cipher text is:\t");
+	for(i=0;i<len;i++)
 		printf("%c",cipher[i]);
 	printf("\n");
-	printf("Decipher:y=%2dx+%2d\n",s,t);
-	for(k=0;k<MAXSIZE;k++)
+	for(j=0;j<len;j++)
 	{
-		y=(int)cipher[k]-(int)'A';
-		x=(s*y+t)%26;
-		plain[k]=((x==23)?' ':(char)(x+97));
+		y=(int)cipher[j]-(int)'A';//assigning 0-25 values
+		x=(c*y+d)%26;//Decryption function
+		plain[j]=((char)(x+97));//converting to lowercase
 	}
-	printf("Decryipted message: \n");
-	for(i=0;i<MAXSIZE;i++)
+	printf("Decrypting...\n");
+	printf("Decryipted text is:\t");
+	for(i=0;i<len;i++)
 		printf("%c",plain[i]);
 		printf("\n");
-		printf("\n");
-
 }
-
